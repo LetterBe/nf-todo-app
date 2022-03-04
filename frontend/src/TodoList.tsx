7d02693c120f77dfbaf88f1e112a0a6be6c260e6
Bárbara <barbarap@tutanota.com>
2022-03-04 17:42:16 +0100

    part 1

--- frontend/src/TodoList.tsx
+++ frontend/src/TodoList.tsx
@@ -9,13 +9,27 @@ import TodoItem from "./TodoItem";
 
 export default function TodoList() {
     const [todos, setTodos] = useState([] as Todo[]);
+    const [errorMessage, setErrorMessage] = useState('')
 
 
     const fetchAll = () => {
         fetch(`${process.env.REACT_APP_BASE_URL}/todos`)
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                }
+                throw new Error("oopps, did not work !")
+            })
+            .then((todosFromBackend: Todo[]) => setTodos(todosFromBackend))
+            .catch ((e: Error) => setErrorMessage(e.message))
+    };
+    const deleteChecked = () => {
+        fetch('${process.env.REACT_APP_BASE_URL}/todos',
+            {method:'DELETE'})
             .then(response => response.json())
-            .then ((todosFromBackend: Todo[]) => setTodos(todosFromBackend));
+            .then((todosFromBackend : Todo[]) => setTodos(todosFromBackend));
     }
+
 
     useEffect(() => {
         fetchAll();
@@ -25,11 +39,20 @@ export default function TodoList() {
         return (
 
             <div>
-                <TodoForm onTodoCreation={setTodos} />
-                {todos.map(todo => <TodoItem key={todo.id} todo={todo}
+                <div>
+                    <TodoForm onTodoCreation={setTodos} />
+                </div>
+                <div>
+                    <button onClick={deleteChecked}>done tasks erase!</button>
+                </div>
+                {todos.map(todo => <li key={todo.id}> <TodoItem todo={todo}
                                              onTodoDelete={fetchAll}
-                                             onTodoChange={setTodos} />)}
+                                             onTodoChange={setTodos} />
+                                    </li>)}
+                <div>
+                    <h2> {errorMessage}</h2>
+                </div>
             </div>
-
         );
   };
+
